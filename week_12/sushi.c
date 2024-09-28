ae3eb8f4759a13896b41c8928289068f91f2665d
parinya-ao <flim.parinya.ao@gmail.com>
2024-09-28 11:14:23 +0700

    sucess week 12

--- week_12/sushi.c
+++ week_12/sushi.c
@@ -1,148 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
+void longestIncreasingSubsequence(double *weights, int *prices, int n)
 {
-    int first;
-    int *second;
-    int second_size;
-} Pair;
+    int *predecessors = (int *)malloc(n * sizeof(int));
+    int *lisEnd = (int *)malloc((n + 1) * sizeof(int));
+    for (int i = 0; i <= n; i++)
+        lisEnd[i] = -1;
+    long length = 0;
 
-void merge(Pair *arr, int left, int mid, int right)
-{
-    int i, j, k;
-    int n1 = mid - left + 1;
-    int n2 = right - mid;
-
-    Pair *L = (Pair *)malloc(n1 * sizeof(Pair));
-    Pair *R = (Pair *)malloc(n2 * sizeof(Pair));
-
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[mid + 1 + j];
-
-    i = 0;
-    j = 0;
-    k = left;
-
-    while (i < n1 && j < n2)
+    for (long i = 0; i < n; i++)
     {
-        if (L[i].first >= R[j].first)
+        long low = 1, high = length + 1;
+
+        while (low < high)
         {
-            arr[k] = L[i];
-            i++;
+            long mid = low + (high - low) / 2;
+            if (weights[lisEnd[mid]] > weights[i])
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid + 1;
+            }
         }
-        else
+
+        long lisLength = low;
+        predecessors[i] = lisEnd[lisLength - 1];
+        lisEnd[lisLength] = i;
+
+        if (lisLength > length)
         {
-            arr[k] = R[j];
-            j++;
+            length = lisLength;
         }
-        k++;
     }
 
-    while (i < n1)
+    int *resultSequence = (int *)malloc(length * sizeof(int));
+    long k = lisEnd[length];
+
+    for (long j = length - 1; j >= 0; j--)
     {
-        arr[k] = L[i];
-        i++;
-        k++;
+        resultSequence[j] = k;
+        k = predecessors[k];
     }
 
-    while (j < n2)
+    int *binaryResult = (int *)malloc(n * sizeof(int));
+    for (int j = 0; j < n; j++)
     {
-        arr[k] = R[j];
-        j++;
-        k++;
+        binaryResult[j] = 0;
     }
-
-    free(L);
-    free(R);
-}
-
-void mergeSort(Pair *arr, int left, int right)
-{
-    if (left < right)
+    int totalPrice = 0;
+    for (int j = 0; j < length; j++)
     {
-        int mid = left + (right - left) / 2;
-
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+        binaryResult[resultSequence[j]] = 1;
+        totalPrice += prices[resultSequence[j]];
     }
+
+    for (int j = 0; j < n; j++)
+    {
+        printf("%d ", binaryResult[j]);
+    }
+    printf("\n");
+
+    printf("%d\n", totalPrice);
+
+    free(predecessors);
+    free(lisEnd);
+    free(resultSequence);
+    free(binaryResult);
 }
 
 int main()
 {
     int n;
     scanf("%d", &n);
-    double *wight = (double *)malloc(n * sizeof(double));
-    int *price = (int *)malloc(n * sizeof(int));
+    double *weights = (double *)malloc(n * sizeof(double));
+    int *prices = (int *)malloc(n * sizeof(int));
 
-    int price1, w;
+    int priceInput, weightInput;
     for (int i = 0; i < n; i++)
     {
-        scanf("%d %d", &price1, &w);
-        wight[i] = (double)w / (double)price1;
-        price[i] = price1;
+        scanf("%d %d", &priceInput, &weightInput);
+        weights[i] = (double)weightInput / (double)priceInput;
+        prices[i] = priceInput;
     }
 
-    Pair *LTS = (Pair *)malloc(n * sizeof(Pair));
-    for (int i = 0; i < n; i++)
-    {
-        LTS[i].first = 1;
-        LTS[i].second = (int *)malloc(n * sizeof(int));
-        LTS[i].second_size = 0;
-    }
+    longestIncreasingSubsequence(weights, prices, n);
 
-    for (int i = n - 1; i >= 0; i--)
-    {
-        LTS[i].second[LTS[i].second_size++] = i;
-        for (int j = 1 + i; j < n; ++j)
-        {
-            if (wight[i] < wight[j])
-            {
-                if (LTS[i].first < LTS[j].first + 1)
-                {
-                    LTS[i].first = 1 + LTS[j].first;
-                    for (int k = 0; k < LTS[j].second_size; k++)
-                    {
-                        LTS[i].second[k] = LTS[j].second[k];
-                    }
-                    LTS[i].second[LTS[j].second_size] = i;
-                    LTS[i].second_size = LTS[j].second_size + 1;
-                }
-            }
-        }
-    }
-
-    mergeSort(LTS, 0, n - 1);
-
-    int *ans = (int *)calloc(n, sizeof(int));
-    int sum = 0;
-    for (int j = 0; j < LTS[0].second_size; j++)
-    {
-        ans[LTS[0].second[j]] = 1;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d ", ans[i]);
-        if (ans[i] == 1)
-        {
-            sum += price[i];
-        }
-    }
-    printf("\n");
-    printf("%d\n", sum);
-
-    free(wight);
-    free(price);
-    for (int i = 0; i < n; i++)
-    {
-        free(LTS[i].second);
-    }
-    free(LTS);
-    free(ans);
-
+    free(weights);
+    free(prices);
     return 0;
 }
